ebbde904d1546d0ab6704e7ef10485fadb051b0f
AntonC9018 <anton.curm@gmail.com>
2019-10-12 20:13:52 +0300

    Example

---
+++ Example/3_1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+float a, b, t;
+float w;
+
+int main() {
+    puts("Introduceti variabile a, b, t:");
+    scanf("%f%f%f", &a, &b, &t);
+
+
+    if (t < 0.1) {
+        w = sqrt(a * t * t + b * sin(t) + 1);
+    } else if (t > 0.1) {
+        w = sqrt(a * t * t + b * cos(t) + 1);
+    } else {
+        w = a * t + b;
+    }
+
+    printf("W = %f", 3.3f);
+
+    return 0;
+}
